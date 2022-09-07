d21b255d9295cc8c4cdbd6eb06a2ccfc77d022b0
Vivek Sahu <viveksahu23@gmail.com>
2022-09-07 15:50:59 +0530

    progress-bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [progress, setProgress] = useState(0);
+  const progressInterval = useRef();
+
+  useEffect(() => {
+    progressInterval.current = setInterval(() => {
+      setProgress((prev) => prev + 1);
+    }, 100);
+  }, []);
+
+  useEffect(() => {
+    if (progress >= 100) {
+      clearInterval(progressInterval.current);
+    }
+  }, [progress]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div style={{ width: progress + "%" }} className="progress-bar">
+        {progress + "%"}
+      </div>
     </div>
   );
 }
-
-export default App;
